de4e125739b303d5d5a97057db25923e0617e26b
seule95 <seulee95@naver.com>
2020-04-01 17:20:13 +0900

    Modify sql file by Seul 04/01

--- MySQL/database_query.sql
+++ MySQL/database_query.sql
@@ -4,7 +4,7 @@ use pentacore;
 
 /***테이블 생성***/
 create table forklift(forkid varchar(10) not null, wareid varchar(10), forkpurdate date, forkmodel varchar(20), forklastcheckdate date, forkdist int(1));
-create table item(itemid varchar(10), wareid varchar(10), itemname varchar(30), warename varchar(30), itemcate varchar(30), itemprice double, itemstock int(1), itemweightpb int(1), itemquantitypb int(1), itemloc varchar(10));
+create table item(itemid varchar(10), wareid varchar(10), itemname varchar(30), warename varchar(30), itemcate varchar(30), itemprice double, itemstock int(1), itemweightpb double, itemqtypb int(1), itemloc varchar(10));
 create table warehouse(wareid varchar(10), warename varchar(30), warecate varchar(30), warescale double, waretype varchar(10));
 
 /***데이터 추가***/
